f563d61685342d3026436b7d4a91f263e68b5b3b
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-18 02:20:05 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student.Change();
+        Student s1 = new Student(1, "Viet");
+        Student s2 = new Student(2, "Vu");
+        Student s3 = new Student(3, "Chien");
+
+        s1.Display();
+        s2.Display();
+        s3.Display();
+    }
+}
